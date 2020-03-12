ef0fd6b81dd4d6364722925e94077c8de7193291
adonson <adonson@users.noreply.github.com>
2020-03-12 15:29:35 +0900

    developer、cpu_socketテーブルのカラム変更
    マイグレーションの順番により、CPUのマイグレーションにdeveloperとcpu_socketを外部キーとして記載できないので、後に変更ファイルとして作成（現在は該当箇所をコメントアウト）

--- db/migrate/20200312060400_create_developers.rb
+++ db/migrate/20200312060400_create_developers.rb
@@ -2,7 +2,6 @@ class CreateDevelopers < ActiveRecord::Migration[5.2]
   def change
     create_table :developers do |t|
       t.string :name, unique: true, null: false
-      t.timestamps
     end
   end
 end
--- db/migrate/20200312061253_create_cpu_sockets.rb
+++ db/migrate/20200312061253_create_cpu_sockets.rb
@@ -2,7 +2,6 @@ class CreateCpuSockets < ActiveRecord::Migration[5.2]
   def change
     create_table :cpu_sockets do |t|
       t.string :name, unique: true, null: false
-      t.timestamps
     end
   end
 end
