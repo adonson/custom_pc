ea8ecbdb7f6b6bcdb4415002d20c0e398f33ada6
adonson <adonson@users.noreply.github.com>
2020-03-13 21:02:21 +0900

    seriesテーブルのseedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,54 @@
-# Developer.create!(
-#  [ 
-#   {id: 1, name: 'INTEL'},
-#   {id: 2, name: 'AMD'},
-#   {id: 3, name: 'NVIDIA'}
-#  ]
-# )
-# CpuSocket.create!(
-#  [ 
-#   {id: 1, name: 'LGA3647'},
-#   {id: 2, name: 'LGA2066'},
-#   {id: 3, name: 'LGA1151'},
-#   {id: 4, name: 'LGA1150'},
-#   {id: 5, name: 'LGA1155'},
-#   {id: 6, name: 'Socket sTRX4'},
-#   {id: 7, name: 'Socket TR4'},
-#   {id: 8, name: 'Socket AM4'},
-#   {id: 9, name: 'LGA2011-3'},
-#   {id: 10, name: 'LGA775'},
-#   {id: 11, name: 'LGA2011'},
-#   {id: 12, name: 'LGA1156'},
-#   {id: 13, name: 'Socket P'},
-#   {id: 14, name: 'Socket F'},
-#   {id: 15, name: 'Socket 771'},
-#   {id: 16, name: 'Socket AM3+'}
-#  ]
-# )
+# ーーーーーーforeign_keyーーーーーー
+
+Developer.create!(
+ [ 
+  {id: 1, name: 'INTEL'},
+  {id: 2, name: 'AMD'},
+  {id: 3, name: 'NVIDIA'}
+ ]
+)
+
+CpuSocket.create!(
+ [ 
+  {id: 1, name: 'LGA3647'},
+  {id: 2, name: 'LGA2066'},
+  {id: 3, name: 'LGA1151'},
+  {id: 4, name: 'LGA1150'},
+  {id: 5, name: 'LGA1155'},
+  {id: 6, name: 'Socket sTRX4'},
+  {id: 7, name: 'Socket TR4'},
+  {id: 8, name: 'Socket AM4'},
+  {id: 9, name: 'LGA2011-3'},
+  {id: 10, name: 'LGA775'},
+  {id: 11, name: 'LGA2011'},
+  {id: 12, name: 'LGA1156'},
+  {id: 13, name: 'Socket P'},
+  {id: 14, name: 'Socket F'},
+  {id: 15, name: 'Socket 771'},
+  {id: 16, name: 'Socket AM3+'}
+ ]
+)
+
+Series.create!(
+ [ 
+  {id: 1, name: 'Xeon'},
+  {id: 2, name: 'Xeon Platinum'},
+  {id: 3, name: 'Xeon Gold'},
+  {id: 4, name: 'Xeon Silver'},
+  {id: 5, name: 'Xeon Bronze'},
+  {id: 6, name: 'Core i9'},
+  {id: 7, name: 'Core i7'},
+  {id: 8, name: 'Core i5'},
+  {id: 9, name: 'Core i3'},
+  {id: 10, name: 'Pentium'},
+  {id: 11, name: 'EPYC'},
+  {id: 12, name: 'Ryzen Threadripper'},
+  {id: 13, name: 'Ryzen 9'},
+  {id: 14, name: 'Ryzen 7'},
+  {id: 15, name: 'Ryzen 5'},
+  {id: 16, name: 'Ryzen 3'},
+  {id: 17, name: 'Athlon'}
+ ]
+)
+
+# ーーーーーーーーーーーーーー
