ac8604dcbaa890c3acdfcbd45920b8c994f803d6
adonson <adonson@users.noreply.github.com>
2020-03-13 21:35:28 +0900

    デモデータ作成
    seedファイルにCPUテーブルのデモデータを作成

--- app/models/cpu.rb
+++ app/models/cpu.rb
@@ -1,5 +1,6 @@
 class Cpu < ApplicationRecord
   belongs_to :developer
   belongs_to :cpu_socket
+  belongs_to :series
   validates  :name, presence:true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,62 @@
 # ーーーーーーforeign_keyーーーーーー
 
-Developer.create!(
- [ 
-  {id: 1, name: 'INTEL'},
-  {id: 2, name: 'AMD'},
-  {id: 3, name: 'NVIDIA'}
- ]
-)
+# Developer.create!(
+#  [ 
+#   {id: 1, name: 'INTEL'},
+#   {id: 2, name: 'AMD'},
+#   {id: 3, name: 'NVIDIA'}
+#  ]
+# )
 
-CpuSocket.create!(
- [ 
-  {id: 1, name: 'LGA3647'},
-  {id: 2, name: 'LGA2066'},
-  {id: 3, name: 'LGA1151'},
-  {id: 4, name: 'LGA1150'},
-  {id: 5, name: 'LGA1155'},
-  {id: 6, name: 'Socket sTRX4'},
-  {id: 7, name: 'Socket TR4'},
-  {id: 8, name: 'Socket AM4'},
-  {id: 9, name: 'LGA2011-3'},
-  {id: 10, name: 'LGA775'},
-  {id: 11, name: 'LGA2011'},
-  {id: 12, name: 'LGA1156'},
-  {id: 13, name: 'Socket P'},
-  {id: 14, name: 'Socket F'},
-  {id: 15, name: 'Socket 771'},
-  {id: 16, name: 'Socket AM3+'}
- ]
-)
+# CpuSocket.create!(
+#  [ 
+#   {id: 1, name: 'LGA3647'},
+#   {id: 2, name: 'LGA2066'},
+#   {id: 3, name: 'LGA1151'},
+#   {id: 4, name: 'LGA1150'},
+#   {id: 5, name: 'LGA1155'},
+#   {id: 6, name: 'Socket sTRX4'},
+#   {id: 7, name: 'Socket TR4'},
+#   {id: 8, name: 'Socket AM4'},
+#   {id: 9, name: 'LGA2011-3'},
+#   {id: 10, name: 'LGA775'},
+#   {id: 11, name: 'LGA2011'},
+#   {id: 12, name: 'LGA1156'},
+#   {id: 13, name: 'Socket P'},
+#   {id: 14, name: 'Socket F'},
+#   {id: 15, name: 'Socket 771'},
+#   {id: 16, name: 'Socket AM3+'}
+#  ]
+# )
 
-Series.create!(
- [ 
-  {id: 1, name: 'Xeon'},
-  {id: 2, name: 'Xeon Platinum'},
-  {id: 3, name: 'Xeon Gold'},
-  {id: 4, name: 'Xeon Silver'},
-  {id: 5, name: 'Xeon Bronze'},
-  {id: 6, name: 'Core i9'},
-  {id: 7, name: 'Core i7'},
-  {id: 8, name: 'Core i5'},
-  {id: 9, name: 'Core i3'},
-  {id: 10, name: 'Pentium'},
-  {id: 11, name: 'EPYC'},
-  {id: 12, name: 'Ryzen Threadripper'},
-  {id: 13, name: 'Ryzen 9'},
-  {id: 14, name: 'Ryzen 7'},
-  {id: 15, name: 'Ryzen 5'},
-  {id: 16, name: 'Ryzen 3'},
-  {id: 17, name: 'Athlon'}
- ]
-)
+# Series.create!(
+#  [ 
+#   {id: 1, name: 'Xeon'},
+#   {id: 2, name: 'Xeon Platinum'},
+#   {id: 3, name: 'Xeon Gold'},
+#   {id: 4, name: 'Xeon Silver'},
+#   {id: 5, name: 'Xeon Bronze'},
+#   {id: 6, name: 'Core i9'},
+#   {id: 7, name: 'Core i7'},
+#   {id: 8, name: 'Core i5'},
+#   {id: 9, name: 'Core i3'},
+#   {id: 10, name: 'Pentium'},
+#   {id: 11, name: 'EPYC'},
+#   {id: 12, name: 'Ryzen Threadripper'},
+#   {id: 13, name: 'Ryzen 9'},
+#   {id: 14, name: 'Ryzen 7'},
+#   {id: 15, name: 'Ryzen 5'},
+#   {id: 16, name: 'Ryzen 3'},
+#   {id: 17, name: 'Athlon'}
+#  ]
+# )
 
 # ーーーーーーーーーーーーーー
+
+Cpu.create!(
+ [ 
+  # {id: 1, name: 'Ryzen 5 2600 BOX', price: 12980, core: 6, thread: 12, clockspeed: 3.4, turbospeed: 3.9, tdp: 65, image: '2600x', gpu: '', release_g: 20180419, release_ja: 20180419, r15: 1298, r20: 2798, developer_id: 2, cpu_socket_id: 8, series_id: 15},
+  {id: 2, name: '', price: , core: , thread: , clockspeed: , turbospeed: , tdp: , image: '', gpu: '', release_g: , release_ja: , r15: , r20: , developer_id: , cpu_socket_id: , series_id: }
+
+ ]
+)
