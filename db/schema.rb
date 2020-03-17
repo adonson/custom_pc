1c1d1184cdeccc334825b309b7c709a4e0a0f99f
adonson <adonson@users.noreply.github.com>
2020-03-17 19:18:10 +0900

    ヘッダーのリンク変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_083400) do
+ActiveRecord::Schema.define(version: 2020_03_17_101157) do
+
+  create_table "bus_interfaces", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+  end
 
   create_table "chip_sets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -132,6 +136,14 @@ ActiveRecord::Schema.define(version: 2020_03_17_083400) do
     t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "videocard_chips", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+  end
+
+  create_table "videocard_memories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
   end
 
   create_table "videocards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
