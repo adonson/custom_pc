ee77092839d475394121b4e2166c4b3f32b6aa39
adonson <adonson@users.noreply.github.com>
2020-03-19 14:35:02 +0900

    Update maker.rb

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -1,6 +1,8 @@
 class Maker < ApplicationRecord
   has_many  :mother_board
+  has_many  :memory
   has_many  :videocard
-  has_many  :case
+  has_many  :power_unit
+  has_many  :ssd
   validates :name, presence:true
 end
