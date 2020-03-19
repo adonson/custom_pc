cb8d1d884f7b2c496fde40ba17f5d0006a9a95d2
adonson <adonson@users.noreply.github.com>
2020-03-19 12:46:53 +0900

    power_unitコントローラー作成

---
+++ app/controllers/power_unit_controller.rb
@@ -0,0 +1,10 @@
+class PowerUnitController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:show]
+  def index
+    @cpus = Cpu.all
+  end
+
+  def show
+    @cpus = Cpu.find(params[:id])
+  end
+end
