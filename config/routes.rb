2a0f5a582a0d3e9627812875bf3b3e20f3f0ae5e
adonson <adonson@users.noreply.github.com>
2020-03-20 14:15:27 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   resources :case, only: [:index, :show]
   resources :power_unit, only: [:index, :show]
   resources :ssd, only: [:index, :show]
+  resources :cpu_cooler, only: [:index, :show]
 end
