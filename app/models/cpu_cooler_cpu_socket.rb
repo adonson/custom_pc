b35fd8dfea2992bd4566178907e7bd034601222f
adonson <adonson@users.noreply.github.com>
2020-03-20 13:43:17 +0900

    cpu_cooler_cpu_socketモデル、マイグレーションファイル作成
    cpu_cooler_cpu_sockeは中間テーブル

---
+++ app/models/cpu_cooler_cpu_socket.rb
@@ -0,0 +1,4 @@
+class CpuCoolerCpuSocket < ApplicationRecord
+  belongs_to :cpu_cooler
+  belongs_to :cpu_socket
+end
---
+++ test/models/cpu_cooler_cpu_socket_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CpuCoolerCpuSocketTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
