1ae0043bb666e245345040caa854d55bc68d72aa
adonson <adonson@users.noreply.github.com>
2020-03-26 20:25:14 +0900

    マイグレーションファイル作成(videocard)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_111614) do
+ActiveRecord::Schema.define(version: 2020_03_26_112024) do
 
   create_table "bus_interfaces", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
