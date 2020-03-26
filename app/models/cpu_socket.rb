e399c4e012b32e9811f7fc6e4ce4ff518e13a7e0
adonson <adonson@users.noreply.github.com>
2020-03-26 15:26:38 +0900

    ER図作成

--- app/models/cpu_socket.rb
+++ app/models/cpu_socket.rb
@@ -1,5 +1,7 @@
 class CpuSocket < ApplicationRecord
   has_many  :cpu
   has_many  :mother_board
+  has_many  :cpu_cooler_cpu_socket
+  has_many  :cpu_cooler, through: :cpu_cooler_cpu_socket
   validates :name, presence:true
 end
