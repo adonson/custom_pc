07b2c110652993f60676a63e97da1b10be2409b1
adonson <adonson@users.noreply.github.com>
2021-01-27 21:15:32 +0900

    cpu_socketを出力

--- app/models/cpu_cooler.rb
+++ app/models/cpu_cooler.rb
@@ -1,9 +1,12 @@
 class CpuCooler < ApplicationRecord
   belongs_to  :maker
   belongs_to  :cpu_cooler_type
-  belongs_to  :cpu_cooler_cpu_socket
+  has_many    :cpu_cooler_cpu_sockets
   has_many    :pc
-  has_many  :cpu_socket, through: :cpu_cooler_cpu_socket
-  accepts_nested_attributes_for :cpu_cooler_cpu_socket
+  has_many    :cpu_sockets, through: :cpu_cooler_cpu_sockets
+  accepts_nested_attributes_for :cpu_cooler_cpu_sockets
   validates   :name, presence:true
+  def view_name_and_color
+    self.name + '(' + self.color + ')'
+  end
 end
